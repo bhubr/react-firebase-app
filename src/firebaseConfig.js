dd87715d0ad3823ba776749cb13b57ce5285d33e
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-10 00:58:05 +0100

    Add firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyBnbz9RlbRui0XYzsgNtwE8MOdTRooiUa0',
+  authDomain: 'reactfirebaseapp-35c5a.firebaseapp.com',
+  databaseURL: 'https://reactfirebaseapp-35c5a.firebaseio.com',
+  projectId: 'reactfirebaseapp-35c5a',
+  storageBucket: 'reactfirebaseapp-35c5a.appspot.com',
+  messagingSenderId: '766530614457',
+  appId: '1:766530614457:web:82845dccf65a3cb575d768',
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
 import './index.css';
 import App from './App';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
