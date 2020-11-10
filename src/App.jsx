91ae0389a21e8c57f440e8d46a39f825adaae9ea
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-10 01:14:08 +0100

    Read whole JSON tree from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // Get a reference to the database service
+    const database = firebase.database();
+    // We loaded api.randomuser.me's response data into the firebase db
+    // see randomuser.json at the repo root
+    return database
+      .ref('/')
+      .once('value')
+      .then((snapshot) => {
+        const val = snapshot.val();
+        const { results } = val;
+        const [randomUser] = results;
+        setUser(randomUser);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>React starter template</h1>
       </header>
+      {user && <img src={user.picture.large} alt={user.name.last} />}
     </div>
   );
 }
