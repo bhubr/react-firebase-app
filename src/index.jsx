90438bb0229b7652f8967e43c42ea1d0a413c42b
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-11-10 01:03:19 +0100

    add rtdb

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase/app';
+import 'firebase/database';
 import firebaseConfig from './firebaseConfig';
 import './index.css';
 import App from './App';
